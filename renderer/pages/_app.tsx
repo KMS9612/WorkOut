c9daacf5ae7cf99ee8d540548437a289fe2b8c02
KMS9612 <kimdev9612@gmail.com>
2023-01-30 04:43:18 +0900

    prevButton

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Button, CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "../lib/theme";
 import type { EmotionCache } from "@emotion/cache";
 import createEmotionCache from "../lib/create-emotion-cache";
 import { CacheProvider } from "@emotion/react";
 import styled from "@emotion/styled";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import { useRouter } from "next/router";
+
 const clientSideEmotionCache = createEmotionCache();
 
 type MyAppProps = AppProps & {
@@ -26,18 +29,32 @@ const Wrapper = styled.div`
 export default function MyApp(props: MyAppProps) {
   const { Component, pageProps, emotionCache = clientSideEmotionCache } = props;
 
+  const router = useRouter();
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <title>WorkOut!</title>
       </Head>
-      <Wrapper>
-        <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <ArrowLeftIcon />
+        </Button>
+        <Wrapper>
           <CssBaseline />
           <Component {...pageProps} />
-        </ThemeProvider>
-      </Wrapper>
+        </Wrapper>
+      </ThemeProvider>
     </CacheProvider>
   );
 }
